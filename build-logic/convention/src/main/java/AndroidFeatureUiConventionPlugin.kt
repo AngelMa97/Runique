b374ae5e03ab14a2b56e9226449ef5437ae55e2b
AngelMa97 <78047307+AngelMa97@users.noreply.github.com>
2025-03-15 12:35:27 -0600

    Add AndroidFeatureUiConventionPlugin and apply to presentation layer

---
+++ build-logic/convention/src/main/java/AndroidFeatureUiConventionPlugin.kt
@@ -0,0 +1,18 @@
+import com.angelme.convention.addUiLayerDependencies
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureUiConventionPlugin: Plugin<Project> {
+    override fun apply(project: Project) {
+        project.run {
+            pluginManager.run {
+                apply("runique.android.library.compose")
+            }
+
+            dependencies {
+                addUiLayerDependencies(project)
+            }
+        }
+    }
+}
