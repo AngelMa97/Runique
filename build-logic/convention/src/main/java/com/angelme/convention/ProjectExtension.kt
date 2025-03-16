804a153b1f94b8c3419e2472b2347826de18c08a
AngelMa97 <78047307+AngelMa97@users.noreply.github.com>
2025-03-15 22:09:21 -0600

    Add Runique theming

--- build-logic/convention/src/main/java/com/angelme/convention/ProjectExtension.kt
+++ build-logic/convention/src/main/java/com/angelme/convention/ProjectExtension.kt
@@ -1,7 +1,6 @@
 package com.angelme.convention
 
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
 
