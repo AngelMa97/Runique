86b496bd7e2fe5a109e56c5b42d038f4d1453bf0
AngelMa97 <78047307+AngelMa97@users.noreply.github.com>
2025-03-22 01:04:18 -0600

    Add LoginViewModel.kt logic to login

--- auth/data/src/main/java/com/angelme/auth/data/AuthRepositoryImpl.kt
+++ auth/data/src/main/java/com/angelme/auth/data/AuthRepositoryImpl.kt
@@ -2,13 +2,41 @@ package com.angelme.auth.data
 
 import com.angelme.auth.domain.AuthRepository
 import com.angelme.core.data.networking.post
+import com.angelme.core.domain.AuthInfo
+import com.angelme.core.domain.SessionStorage
 import com.angelme.core.domain.util.DataError
 import com.angelme.core.domain.util.EmptyResult
+import com.angelme.core.domain.util.Result
+import com.angelme.core.domain.util.asEmptyDataResult
 import io.ktor.client.HttpClient
 
 class AuthRepositoryImpl(
-    private val httpClient: HttpClient
-): AuthRepository {
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
+) : AuthRepository {
+
+    override suspend fun login(
+        email: String,
+        password: String
+    ): EmptyResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, LoginResponse>(
+            route = "/login",
+            body = LoginRequest(
+                email = email,
+                password = password
+            )
+        )
+        if (result is Result.Success) {
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken,
+                    userId = result.data.userId
+                )
+            )
+        }
+        return result.asEmptyDataResult()
+    }
 
     override suspend fun register(
         email: String,
