4d266af125a2741f7de93950f0d0ff6ca9a9b30a
AngelMa97 <78047307+AngelMa97@users.noreply.github.com>
2025-03-24 20:23:32 -0600

    Add RunUi.kt - Add mappers to transform Run.kt

---
+++ run/presentation/src/main/java/com/angelme/run/presentation/run_overview/mapper/RunMapper.kt
@@ -0,0 +1,33 @@
+package com.angelme.run.presentation.run_overview.mapper
+
+import com.angelme.core.domain.run.Run
+import com.angelme.core.presentation.ui.toFormattedKm
+import com.angelme.core.presentation.ui.toFormattedKmh
+import com.angelme.core.presentation.ui.toFormattedMeters
+import com.angelme.core.presentation.ui.toFormattedPace
+import com.angelme.run.presentation.run_overview.model.RunUi
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun Run.toRunUi(): RunUi {
+    val dateTimeInLocalTime = dateTimeUtc
+        .withZoneSameInstant(ZoneId.systemDefault())
+
+    val formattedDateTime = DateTimeFormatter
+        .ofPattern("MMM dd, yyyy - hh:mma")
+        .format(dateTimeInLocalTime)
+    
+    val distanceKm = distanceMeters / 1000.0
+    
+    return RunUi(
+        id = id!!,
+        duration = duration.toString(),
+        dateTime = formattedDateTime,
+        distance = distanceKm.toFormattedKm(),
+        avgSpeed = avgSpeedKmh.toFormattedKmh(),
+        maxSpeed = maxSpeedKmh.toFormattedKmh(),
+        pace = duration.toFormattedPace(distanceKm),
+        totalElevation = totalElevationMeters.toFormattedMeters(),
+        mapPictureUrl = mapPictureUrl
+    )
+}
