10c62e95ad26ef01338c884e7c875ba119292b04
AngelMa97 <78047307+AngelMa97@users.noreply.github.com>
2025-03-27 12:59:54 -0600

    Add AnalyticsDashboardScreen.kt

--- build-logic/convention/src/main/java/com/angelme/convention/BuildTypes.kt
+++ build-logic/convention/src/main/java/com/angelme/convention/BuildTypes.kt
@@ -51,6 +51,7 @@ internal fun Project.configureBuildTypes(
                         }
                         release {
                             configureReleaseType(apiKey, commonExtension)
+                            isMinifyEnabled = false
                         }
                     }
                 }
